9c37ade8b4fa6940b3b5a3489fd4a3a0b9cab11c
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-01-28 20:13:30 +0300

    challenge37

---
+++ 37-Pangrams/37-Pangrams/mdlMain.vb
@@ -0,0 +1,21 @@
+﻿Imports System.IO
+Module mdlMain
+    Sub Main(ByVal sArgs() As String)
+        Dim alfabe As String = "abcdefghijklmnopqrstuvwxyz"
+        'dosyayı aç
+        Dim dosyaYolu As String = sArgs(0)
+        Using sr As New StreamReader(dosyaYolu)
+            'satır oldukça
+            While (sr.Peek() <> -1)
+                Dim line As String = sr.ReadLine().ToLower()
+                Dim eksikler As String = ""
+                For i As Integer = 0 To Len(alfabe) - 1
+                    If InStr(line, alfabe.Substring(i, 1)) = 0 Then eksikler &= alfabe.Substring(i, 1)
+                Next
+                If eksikler = "" Then eksikler = "NULL"
+                Console.WriteLine(eksikler)
+            End While
+        End Using
+        Console.ReadLine()
+    End Sub
+End Module
