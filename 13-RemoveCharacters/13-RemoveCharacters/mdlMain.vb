8546e075e0d18c547bc069c6e4bc1d35041e4af8
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-01-28 20:11:14 +0300

    challenge13

---
+++ 13-RemoveCharacters/13-RemoveCharacters/mdlMain.vb
@@ -0,0 +1,21 @@
+﻿Imports System.IO
+Module mdlMain
+    Sub Main(ByVal sArgs() As String)
+        'dosyayı aç
+        Dim dosyaYolu As String = sArgs(0)
+        Using sr As New StreamReader(dosyaYolu)
+            'satır oldukça
+            While (sr.Peek() <> -1)
+                Dim line As String = sr.ReadLine()
+                Dim words() As String = line.Split(",")
+                words(1) = words(1).Trim()
+                For i As Integer = 0 To words(1).Length - 1
+                    words(0) = words(0).Replace(words(1).Substring(i, 1), "")
+
+                Next
+                Console.WriteLine(words(0))
+            End While
+        End Using
+        Console.ReadLine()
+    End Sub
+End Module
