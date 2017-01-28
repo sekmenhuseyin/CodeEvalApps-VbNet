8a7a4ef234670a44e6979975f019ce96a350712b
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-01-28 20:12:08 +0300

    challenge22

---
+++ 22-FibonacciSeries/22-FibonacciSeries/mdlMain.vb
@@ -0,0 +1,22 @@
+﻿Imports System.IO
+Module mdlMain
+    Sub Main(ByVal sArgs() As String)
+        Dim fibonecci(100) As Integer
+        fibonecci(0) = 0
+        fibonecci(1) = 1
+        For i As Integer = 1 To 40
+            fibonecci(i + 1) = fibonecci(i) + fibonecci(i - 1)
+        Next
+        'dosyayı aç
+        Dim dosyaYolu As String = sArgs(0)
+        Using sr As New StreamReader(dosyaYolu)
+            'satır oldukça
+            While (sr.Peek() <> -1)
+                Dim line As String = sr.ReadLine()
+
+                Console.WriteLine(fibonecci(Convert.ToInt32(line)))
+            End While
+        End Using
+        Console.ReadLine()
+    End Sub
+End Module
