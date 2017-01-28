2d68c208e805d3ecb2ed1d2e4803029d047071ed
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-01-28 20:12:52 +0300

    challenge28

---
+++ 28-StringSearching/28-StringSearching/mdlMain.vb
@@ -0,0 +1,25 @@
+﻿Imports System.IO
+Module mdlMain
+    Sub Main(ByVal sArgs() As String)
+        'dosyayı aç
+        Dim dosyaYolu As String = sArgs(0)
+        Using sr As New StreamReader(dosyaYolu)
+            'satır oldukça
+            While (sr.Peek() <> -1)
+                Dim line As String = sr.ReadLine()
+                Dim words() As String = line.Split(",")
+                Dim sonuc As Boolean = words(0).Contains(words(1)) = True
+                If sonuc = False And words(1).Contains("*") = True Then
+                    If words(1).Contains("\*") = True And words(0).Contains("*") = False Then
+                    Else
+                        sonuc = True
+                    End If
+
+                End If
+
+                Console.WriteLine(sonuc.ToString().ToLower())
+            End While
+        End Using
+        Console.ReadLine()
+    End Sub
+End Module
