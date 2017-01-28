5fee5268735d5fa929f9805dd349c293d7826f3f
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-01-28 20:13:50 +0300

    challenge41

---
+++ 41-ArrayAbsurdity/41-ArrayAbsurdity/mdlMain.vb
@@ -0,0 +1,24 @@
+﻿Imports System.IO
+Module mdlMain
+    Sub Main(ByVal sArgs() As String)
+        'dosyayı aç
+        Dim dosyaYolu As String = sArgs(0)
+        Using sr As New StreamReader(dosyaYolu)
+            'satır oldukça
+            While (sr.Peek() <> -1)
+                Dim line As String = sr.ReadLine()
+                If line > "" Then
+                    Dim parts() As String = line.Split(";")
+                    Dim numbers() As String = parts(1).Split(",")
+                    For i As Integer = 0 To UBound(numbers) - 1
+                        For j As Integer = i + 1 To UBound(numbers)
+                            If numbers(i) = numbers(j) Then line = numbers(i) : Exit For
+                        Next
+                    Next
+                End If
+                Console.WriteLine(line)
+            End While
+        End Using
+        Console.ReadLine()
+    End Sub
+End Module
