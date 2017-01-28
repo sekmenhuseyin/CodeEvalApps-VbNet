7d10e8252447f7b10212ee654e62dcfe70be33a1
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-01-28 20:13:03 +0300

    challenge30

---
+++ 30-SetIntersection/30-SetIntersection/mdlMain.vb
@@ -0,0 +1,27 @@
+﻿Imports System.IO
+Module mdlMain
+    Sub Main(ByVal sArgs() As String)
+        'dosyayı aç
+        Dim dosyaYolu As String = sArgs(0)
+        Using sr As New StreamReader(dosyaYolu)
+            'satır oldukça
+            While (sr.Peek() <> -1)
+                Dim line As String = sr.ReadLine()
+                Dim parts() As String = line.Split(";")
+                Dim nums1() As String = parts(0).Split(",")
+                Dim nums2() As String = parts(1).Split(",")
+                Dim sonuc As String = ""
+                For Each item As String In nums1
+                    For Each item2 As String In nums2
+                        If item = item2 Then
+                            If sonuc <> "" Then sonuc &= ","
+                            sonuc &= item
+                        End If
+                    Next
+                Next
+                Console.WriteLine(sonuc)
+            End While
+        End Using
+        Console.ReadLine()
+    End Sub
+End Module
