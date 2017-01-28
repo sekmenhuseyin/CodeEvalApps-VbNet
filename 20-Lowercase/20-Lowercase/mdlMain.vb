9e79331a49a466c9f47106ba032813e7475c78ba
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-01-28 20:11:55 +0300

    challenge20

---
+++ 20-Lowercase/20-Lowercase/mdlMain.vb
@@ -0,0 +1,16 @@
+﻿Imports System.IO
+Module mdlMain
+    Sub Main(ByVal sArgs() As String)
+        'dosyayı aç
+        Dim dosyaYolu As String = sArgs(0)
+        Using sr As New StreamReader(dosyaYolu)
+            'satır oldukça
+            While (sr.Peek() <> -1)
+                Dim line As String = sr.ReadLine()
+
+                Console.WriteLine(LCase(line))
+            End While
+        End Using
+        Console.ReadLine()
+    End Sub
+End Module
