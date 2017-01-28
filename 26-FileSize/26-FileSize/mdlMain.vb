4c772384da8aa4f1189de18fd19e876dd616f911
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-01-28 20:12:38 +0300

    challenge26

---
+++ 26-FileSize/26-FileSize/mdlMain.vb
@@ -0,0 +1,8 @@
+﻿Imports System.IO
+Module mdlMain
+    Sub Main(ByVal sArgs() As String)
+        Dim myFile As New FileInfo(sArgs(0))
+        Console.WriteLine(myFile.Length)
+        Console.ReadLine()
+    End Sub
+End Module
