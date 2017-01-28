1b018e49c72100ac40ad7eb38c5d4077b6eee7a7
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-01-28 20:12:32 +0300

    challenge25

---
+++ 25-OddNumbers/25-OddNumbers/mdlMain.vb
@@ -0,0 +1,9 @@
+﻿Imports System.IO
+Module mdlMain
+    Sub Main(ByVal sArgs() As String)
+        For i As Integer = 1 To 99
+            If i Mod 2 = 1 Then Console.WriteLine(i)
+        Next
+        Console.ReadLine()
+    End Sub
+End Module
