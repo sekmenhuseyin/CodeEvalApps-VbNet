6f3ac2fb0dd21ccf5ffd333fdeb40b1f82f44da7
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-01-28 20:13:09 +0300

    challenge31

---
+++ 31-RightMostChar/31-RightMostChar/Main.vb
@@ -0,0 +1,20 @@
+﻿Imports System.IO
+Module Main
+    Sub Main(ByVal sArgs() As String)
+        'dosyayı aç
+        Dim dosyaYolu As String = sArgs(0)
+        Using sr As New StreamReader(dosyaYolu)
+            'satır oldukça
+            While (sr.Peek() <> -1)
+                Dim line As String = sr.ReadLine()
+                Dim words() As String = line.Split(",")
+                Dim sonuc As Integer = -1
+                For i As Integer = 0 To words(0).Length - 1
+                    If words(0).Substring(i, 1) = words(1) Then sonuc = i
+                Next
+                Console.WriteLine(sonuc)
+            End While
+        End Using
+        Console.ReadLine()
+    End Sub
+End Module
---
+++ 31-RightMostChar/31-RightMostChar/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("31-RightMostChar")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("31-RightMostChar")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e47745d4-4dfd-44f5-91ae-5a71abff8751")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
