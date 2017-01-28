e0ac1524b3b8e73fd06795c2a48ea3dfe946821e
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-01-28 20:12:14 +0300

    challenge23

---
+++ 23-MultiplicationTables/23-MultiplicationTables/mdlMain.vb
@@ -0,0 +1,13 @@
+﻿Imports System.IO
+Module mdlMain
+    Sub Main(ByVal sArgs() As String)
+        For i As Integer = 1 To 12
+            Dim line As String = ""
+            For j As Integer = 1 To 12
+                line &= Right("   " & (i * j), 4)
+            Next
+            Console.WriteLine(line)
+        Next
+        Console.ReadLine()
+    End Sub
+End Module
