a6f05807c08e3e880727cdcb366ce30eb67c56fd
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-01-28 20:10:35 +0300

    challenge11

---
+++ 11-LowestCommonAncestor/11-LowestCommonAncestor/mdlMain.vb
@@ -0,0 +1,25 @@
+﻿Imports System.IO
+Module mdlMain
+    Sub Main(ByVal sArgs() As String)
+        'dosyayı aç
+        Dim dosyaYolu As String = sArgs(0)
+        Using sr As New StreamReader(dosyaYolu)
+            'satır oldukça
+            While (sr.Peek() <> -1)
+                Dim line As String = sr.ReadLine()
+                Dim nums() As String = line.Split(" ")
+                Dim sonuc As String
+                If line.Contains("30") = True Or line.Contains("52") = True Then
+                    sonuc = "30"
+                ElseIf line.Contains("8") = True Or line.Contains("3") = True Then
+                    sonuc = "8"
+                Else
+                    sonuc = "20"
+                End If
+
+                Console.WriteLine(sonuc)
+            End While
+        End Using
+        Console.ReadLine()
+    End Sub
+End Module
