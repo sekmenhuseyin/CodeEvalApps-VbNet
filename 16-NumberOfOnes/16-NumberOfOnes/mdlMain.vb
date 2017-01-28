2cb27848838f501c6535be9eb76f72a0bbfe7eb1
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-01-28 20:11:29 +0300

    challenge16

---
+++ 16-NumberOfOnes/16-NumberOfOnes/mdlMain.vb
@@ -0,0 +1,20 @@
+﻿Imports System.IO
+Module mdlMain
+    Sub Main(ByVal sArgs() As String)
+        'dosyayı aç
+        Dim dosyaYolu As String = sArgs(0)
+        Using sr As New StreamReader(dosyaYolu)
+            'satır oldukça
+            While (sr.Peek() <> -1)
+                Dim line As String = sr.ReadLine()
+                line = Convert.ToString(Convert.ToInt32(line), 2)
+                Dim sayac As Integer = 0
+                For i As Integer = 0 To line.Length - 1
+                    If line.Substring(i, 1) = "1" Then sayac += 1
+                Next
+                Console.WriteLine(sayac)
+            End While
+        End Using
+        Console.ReadLine()
+    End Sub
+End Module
