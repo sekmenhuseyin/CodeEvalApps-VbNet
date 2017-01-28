a99814d65c9e02c85dc043d2be3dc63e2af75657
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-01-28 20:16:50 +0300

    challenge 66-67-68

---
+++ 67-HexToDec/67-HexToDec/Main.vb
@@ -0,0 +1,25 @@
+﻿Imports System.IO
+Module Main
+
+    Sub Main(ByVal sArgs() As String)
+
+        'dosyayı aç
+        Dim dosyaYolu As String = sArgs(0)
+        Using sr As New StreamReader(dosyaYolu)
+            'satır oldukça
+            While (sr.Peek() <> -1)
+                Dim line As String = sr.ReadLine()
+                Dim sonuc As Integer = 0 : Dim carpan As Integer = 1
+                'sayının basamaklarını bul
+                For i As Integer = line.Length - 1 To 0 Step -1
+                    Dim tmpx = line.Substring(i, 1).Replace("a", "10").Replace("b", "11").Replace("c", "12").Replace("d", "13").Replace("e", "14").Replace("f", "15")
+                    sonuc += carpan * Convert.ToInt32(tmpx)
+                    carpan *= 16
+                Next
+                Console.WriteLine(sonuc)
+            End While
+        End Using
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ 68-ValidParentheses/68-ValidParentheses/mdlMain.vb
@@ -0,0 +1,44 @@
+﻿Imports System.IO
+Module mdlMain
+    Sub Main(ByVal sArgs() As String)
+        Dim parantez(2, 1) As Integer '() [] {}
+        'dosyayı aç
+        Dim dosyaYolu As String = sArgs(0)
+        Using sr As New StreamReader(dosyaYolu)
+            'satır oldukça
+            While (sr.Peek() <> -1)
+                Dim line As String = sr.ReadLine()
+                For i As Integer = 0 To 2
+                    For j As Integer = 0 To 1
+                        parantez(i, j) = 0
+                    Next
+                Next
+                For i As Integer = 0 To line.Length - 1
+                    If line(i) = "(" Then
+                        parantez(0, 0) += 1
+                    ElseIf line(i) = ")" Then
+                        parantez(0, 1) += 1
+                    ElseIf line(i) = "[" Then
+                        parantez(1, 0) += 1
+                    ElseIf line(i) = "]" Then
+                        parantez(1, 1) += 1
+                    ElseIf line(i) = "{" Then
+                        parantez(2, 0) += 1
+                    ElseIf line(i) = "}" Then
+                        parantez(2, 1) += 1
+                    Else
+                    End If
+                Next
+                'control
+                Dim sonuc As Boolean = True
+                For i As Integer = 0 To 2
+                    If parantez(i, 0) <> parantez(i, 1) Then sonuc = False
+                Next
+                If line.Contains("[)") = True Or line.Contains("(}") = True Then sonuc = False
+                'return
+                Console.WriteLine(sonuc)
+            End While
+        End Using
+        Console.ReadLine()
+    End Sub
+End Module
