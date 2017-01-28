ca7f8de93d2ac88da8989a853a885daeabee5988
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-01-28 20:12:21 +0300

    challenge24

---
+++ 24-SumOfIntegersFromFile/24-SumOfIntegersFromFile/mdlMain.vb
@@ -0,0 +1,18 @@
+﻿Imports System.IO
+Module mdlMain
+    Sub Main(ByVal sArgs() As String)
+        'dosyayı aç
+        Dim dosyaYolu As String = sArgs(0)
+        Dim sonuc As Integer = 0
+        Using sr As New StreamReader(dosyaYolu)
+            'satır oldukça
+            While (sr.Peek() <> -1)
+                Dim line As String = sr.ReadLine()
+                sonuc += Convert.ToInt32(line)
+
+            End While
+        End Using
+        Console.WriteLine(sonuc)
+        Console.ReadLine()
+    End Sub
+End Module
