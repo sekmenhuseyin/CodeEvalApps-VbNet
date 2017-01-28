8029f5a2f91dae7e311663f062ff445dd8decca3
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-01-28 20:10:50 +0300

    challenge12

---
+++ 12-FirstNonRepeatedCharacter/12-FirstNonRepeatedCharacter/mdlMain.vb
@@ -0,0 +1,20 @@
+﻿Imports System.IO
+Module mdlMain
+    Sub Main(ByVal sArgs() As String)
+        'dosyayı aç
+        Dim dosyaYolu As String = sArgs(0)
+        Using sr As New StreamReader(dosyaYolu)
+            'satır oldukça
+            While (sr.Peek() <> -1)
+                Dim line As String = sr.ReadLine()
+                Dim tmp As String = "" : Dim sonuc As String = ""
+                For i As Integer = 0 To line.Length - 1
+                    tmp = line.Replace(line.Substring(i, 1), "")
+                    If tmp.Length = line.Length - 1 Then sonuc = line.Substring(i, 1) : Exit For
+                Next
+                Console.WriteLine(sonuc)
+            End While
+        End Using
+        Console.ReadLine()
+    End Sub
+End Module
