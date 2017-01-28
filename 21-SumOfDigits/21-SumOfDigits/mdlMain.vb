1143e587d21beeb88378a6078d731454e99d20a2
Hüseyin Sekmenoğlu <hus_as@yahoo.com>
2017-01-28 20:12:02 +0300

    challenge21

---
+++ 21-SumOfDigits/21-SumOfDigits/mdlMain.vb
@@ -0,0 +1,21 @@
+﻿Imports System.IO
+Module mdlMain
+    Sub Main(ByVal sArgs() As String)
+        'dosyayı aç
+        Dim dosyaYolu As String = sArgs(0)
+        Dim tmp As String
+        Using sr As New StreamReader(dosyaYolu)
+            'satır oldukça
+            While (sr.Peek() <> -1)
+                Dim line As String = sr.ReadLine()
+                Dim sonuc As Integer = 0
+                For i As Integer = 0 To line.Length - 1
+                    tmp = line(i)
+                    sonuc += Convert.ToInt32(tmp)
+                Next
+                Console.WriteLine(sonuc)
+            End While
+        End Using
+        Console.ReadLine()
+    End Sub
+End Module
